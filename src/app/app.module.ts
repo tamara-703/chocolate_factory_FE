fb4d1da5c146ccf89ccc1eac7c4182beb2594a8b
Tamara Saadeh <tamara.saadeh95@gmail.com>
2023-08-18 13:30:32 -0400

    First commit to FE chocolate_factory application. Started an angular project and added the necessary components + service component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ProductsComponent } from './products/products.component';
+import { CollectionsComponent } from './collections/collections.component';
+import { ManufacturerComponent } from './manufacturer/manufacturer.component';
+import { AboutComponent } from './about/about.component';
+import { FetchService } from './fetch.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HomepageComponent,
+    ProductsComponent,
+    CollectionsComponent,
+    ManufacturerComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [FetchService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent {
+
+  //the dashboard component will contain navigations to the homepage component, products component, collections component, manufacturer component, about component
+  //the dahsboard will also have a logo at the far right of the dashboard (navigating to home should be available when clicking on the logo)
+
+}
